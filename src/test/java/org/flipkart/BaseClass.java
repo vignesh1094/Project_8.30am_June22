f302237ec4a6fdaa30af9b22f2e48478769b9b02
vignesh1094 <vigneshsampath01@gmail.com>
2022-06-07 12:26:50 +0530

    product structure

---
+++ src/test/java/org/flipkart/BaseClass.java
@@ -0,0 +1,9 @@
+package org.flipkart;
+
+import org.openqa.selenium.WebElement;
+
+public class BaseClass {
+	
+	public static Web
+
+}
---
+++ src/test/java/org/flipkart/Locators.java
@@ -0,0 +1,5 @@
+package org.flipkart;
+
+public class Locators {
+
+}
